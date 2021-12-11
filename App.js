0098959cb7d385e996ac25cce0a1ac8f8ee9706c
GKTechy <glapskrish@gmail.com>
2021-12-11 12:58:06 +0530

    added the voice dependencies

--- App.js
+++ App.js
@@ -1,12 +1,85 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useEffect,useState} from 'react';
+import { StyleSheet, Text, View,Button,SafeAreaView,Alert  } from 'react-native';
+
+import Voice from '@react-native-voice/voice';
 
 export default function App() {
+  const [result,setResult]=useState([]);
+
+  useEffect(() => {
+    Voice.onSpeechStart = onSpeechStartHandler;
+    Voice.onSpeechEnd = onSpeechEndHandler;
+    Voice.onSpeechResults = onSpeechResultsHandler;
+    
+    console.log("is avilable-->"+JSON.stringify(Voice.isAvailable()))
+    return ()=>{
+      Voice.destroy().then(Voice.removeAllListeners);
+    }
+  },[])
+
+  const onSpeechStartHandler=(e)=>{
+    console.log("onSpeechStartHandler-->"+e)
+  }
+  
+  const onSpeechEndHandler=(e)=>{
+       console.log("onSpeechEndHandler-->"+e)
+  }
+  
+  const onSpeechResultsHandler=(e)=>{
+    let text=e.value;
+    setResult(text);
+    console.log("onSpeechResultsHandler-->"+e)
+  }
+
+  const startRecording= async ()=>{
+    try{
+      console.log("Calling startRecording-->")
+      await Voice.start('en-US');
+    }catch(err){
+      console.log("Error raised"+err)
+    }
+  }
+
+  
+  const stopRecording= async ()=>{
+    try{
+      console.log("Calling stopRecording-->")
+      await Voice.stop();
+    }catch(err){
+      console.log("Error raised"+err)
+    }
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+       <Text>Voice to Text Conversion</Text>
+          
+          <View style={styles.fixToText}>
+              <Button
+                title="Start Recording"
+                onPress={startRecording}
+              />
+              <Text>    </Text>
+              <Button
+                title="Stop Recording"
+                onPress={stopRecording}
+              />
+          </View>
+          <View>
+            <Text>Result:  {result}
+            
+            {result.map((result, index) => {
+						return (
+							<Text
+								key={`result-${index}`}>
+								{result}
+							</Text>
+						);
+					})}
+          </Text>
+          </View>
     </View>
   );
 }
